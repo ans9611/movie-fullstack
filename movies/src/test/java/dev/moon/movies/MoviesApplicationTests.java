ce5e4b1bf0714a35f6e3e1d8a01d49821c310341
ans9611 <moonye1006@gmail.com>
2024-05-31 16:42:15 -0700

    created java framework for backend

---
+++ movies/src/test/java/dev/moon/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.moon.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
