f6c1a1384bd5b3bd17824019c435b92850e79e3e
ans9611 <moonye1006@gmail.com>
2024-05-31 18:30:09 -0700

    successfully pull out data from MongoDB

--- movies/src/main/java/dev/moon/movies/MovieController.java
+++ movies/src/main/java/dev/moon/movies/MovieController.java
@@ -1,5 +1,8 @@
 package dev.moon.movies;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+  @Autowired
+  private MovieService movieService;
   @GetMapping
-  public ResponseEntity<String> allMovies() {
-    return new ResponseEntity<String>("All Mobies", HttpStatus.OK);
+  public ResponseEntity<List<Movie>> getAllMovies() {
+    return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
   }
 }
